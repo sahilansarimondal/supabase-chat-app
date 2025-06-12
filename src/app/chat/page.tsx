f4a0fadcc9acc3efb929513946445911979f95e2
Sahil Ansari Mondal <mondalrajesh761@gmail.com>
2025-06-12 08:17:00 +0530

    removed demo content and add a call to action in chat page

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,40 +1,15 @@
-import { createChat } from "@/lib/actions/chatActions";
-import { getAllUsers } from "@/lib/actions/profileAction";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
+
 import React from "react";
 
 const RoomPage = async () => {
-  const { data } = await getAllUsers();
-
-  const chatCreation = async (formData: FormData) => {
-    "use server";
-    await createChat(formData);
-  };
-
-  if (!data) {
-    return <div>No users found</div>;
-  }
   return (
-    <div className=" flex justify-center items-center min-h-screen flex-col gap-4">
-      {data.map((user) => {
-        return (
-          <form
-            action={chatCreation}
-            className="text-2xl underline cursor-pointer"
-            key={user.id}
-          >
-            <input
-              type="text"
-              name="receiverId"
-              defaultValue={user.id}
-              hidden
-            />
-            <button type="submit" className=" cursor-pointer">
-              {user.full_name}
-            </button>
-          </form>
-        );
-      })}
-    </div>
+    <>
+      <Card className="flex justify-center items-center flex-col h-screen">
+        <CardContent className=""> Select a Chat to continue </CardContent>
+        <CardFooter className="mt-4"></CardFooter>
+      </Card>
+    </>
   );
 };
 
